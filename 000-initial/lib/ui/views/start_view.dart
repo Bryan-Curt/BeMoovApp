0fef338f376289f6782cc34348d86f57aa32ac5b
Bryan Curt <bryancurt@MacBook-Pro-2.local>
2020-10-20 11:53:52 +0200

    menu dialog

--- 000-initial/lib/ui/views/start_view.dart
+++ 000-initial/lib/ui/views/start_view.dart
@@ -21,7 +21,57 @@ class StartView extends StatelessWidget {
     Widget menusection = Container(
       padding: const EdgeInsets.only(top: 20, right: 0, left: 325),
       child: RawMaterialButton(
-        onPressed: () {},
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return Dialog(
+                  insetPadding: EdgeInsets.only(top: 50),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  elevation: 5,
+                  child: Container(
+                    height: 200.0,
+                    width: 360.0,
+                    child: ListView(
+                      children: <Widget>[
+                        SizedBox(height: 20),
+                        FlatButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "Accueil",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 17),
+                          ),
+                        ),
+                        FlatButton(
+                          onPressed: () {
+                            /*...*/
+                          },
+                          child: Text(
+                            "Voir vos précédents entraînements",
+                            style:
+                                TextStyle(color: Colors.black54, fontSize: 17),
+                          ),
+                        ),
+                        FlatButton(
+                          onPressed: () {
+                            /*...*/
+                          },
+                          child: Text(
+                            "Moyenne de vos efforts",
+                            style:
+                                TextStyle(color: Colors.black54, fontSize: 17),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              });
+        },
         elevation: 0,
         fillColor: Colors.transparent,
         splashColor: Colors.black26,
